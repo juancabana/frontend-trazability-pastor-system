1752dd41fa73f67b04dcb9a69a6810ca9c15831b
Juan Cabana <juan.cabana@pragma.com.co>
2026-04-26 11:25:16 -0500

    feature: Style improvements

---
+++ src/components/atoms/PageLoader.tsx
@@ -0,0 +1,7 @@
+export function PageLoader() {
+  return (
+    <div className="flex items-center justify-center min-h-[60vh]">
+      <div className="w-8 h-8 rounded-full border-2 border-teal-500 border-t-transparent animate-spin" />
+    </div>
+  );
+}
